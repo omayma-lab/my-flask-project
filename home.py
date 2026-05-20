6d8f6a83570de57e29b6a918abe9b2edee09da83
omayma <ojaa09411@gmail.com>
2026-05-20 11:48:40 +0200

    test route home

--- home.py
+++ home.py
@@ -1,5 +1,12 @@
 from flask import Blueprint ,render_template ,session ,redirect
-
+from flask import Blueprint
 home =Blueprint ("home",__name__)
-import os
-import mysql.connector
+
+
+@home.route("/home")
+def home_page():
+    return "HOME PAGE 🚀"
+
+@home.route("/")
+def index():
+    return "App is working"
